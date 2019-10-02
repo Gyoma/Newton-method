c201dce5428f017d43cf66f562f632b092a7d6d8
Gyoma <art.golovin.v@gmail.com>
2019-10-02 23:13:08 +0700

    updated

--- Newton/Newton.py
+++ Newton/Newton.py
@@ -3,39 +3,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 x = [ 0.0, 1.5, 3.6, 4.3, 5.0 ]
-y = [ 1.732, 0.931, 1.732, 1.327, 1.289 ]
+y = [ 1.732, 0.931, 1.732, 1.327, 1.289 ] 
 
 pX = 5.251
-
-data = pd.DataFrame(np.array([yy for yy in y]).T, columns = [ 1 ])
-errdata = pd.DataFrame(np.array([0.5e-3 for i in range(len(y))]).T, columns = [ 1 ])
 
 origX = np.linspace(min(x), max(x), 1000)
 origY = (origX + 3 * np.cos(origX) ** 2) ** 0.5 - 0.2 * origX
 
-def Newton ( crdsX, crdsY, x ):
-	res = 0
-	for i in range(1, len(crdsX)):
-		mult = 1.0
-		for j in range(i):
-			mult *= (x - crdsX[j])
-		res += data.loc[i, i + 1] * mult
-	res += crdsY[0]
+def CreateData ( crdsX, crdsY ):
+	
+	data = pd.DataFrame(np.array([yy for yy in crdsY]).T, columns = [ 1 ])
 
-	return res
-
-def AbsError ( crdsX, x ):
-	res = 0
-	for i in range(1, len(crdsX)):
-		mult = 1.0
-		for j in range(i):
-			mult *= abs(x - crdsX[j])
-		res += errdata.loc[i , i + 1] * mult
-	res += 0.5e-3
-
-	return res
-
-def CreateData ( crdsX, indexes ):
+	def Create ( crdsX, indexes ):
 		if len(indexes) == 0:
 			return 0
 		if len(indexes) == 1:
@@ -44,20 +23,31 @@ def CreateData ( crdsX, indexes ):
 			startX = crdsX[indexes[0]]
 			endX = crdsX[indexes[-1]]
 			lngth = len(indexes)
-			val1 = (data.loc[indexes[-1] - 1, lngth - 1] if (data.shape[1] >= lngth and data.loc[indexes[-1] - 1][lngth - 1] is not None)
-					else CreateData(crdsX, indexes[:-1]))
-			val2 = (data.loc[indexes[-1], lngth - 1] if (data.shape[1] >= lngth and data.loc[indexes[-1], lngth - 1] is not None)
-					else CreateData(crdsX, indexes[1:]))
+			
+			val1 = (data.loc[indexes[-1] - 1, lngth - 1] 
+					if (data.shape[1] >= lngth and data.loc[indexes[-1] - 1][lngth - 1] is not None)
+					else Create(crdsX, indexes[:-1]))
+			
+			val2 = (data.loc[indexes[-1], lngth - 1]
+					if (data.shape[1] >= lngth and data.loc[indexes[-1], lngth - 1] is not None)
+					else Create(crdsX, indexes[1:]))
+			
 			val = (val2 - val1) / (endX - startX)
 		
 			if lngth not in data.columns:
 				data[lngth] = None
 
 			data.loc[indexes[-1], lngth] = val
-
 			return val
 
-def CreateErrData ( crdsX, indexes ):
+	Create(crdsX, [i for i in range(len(crdsX))])
+	return [data.loc[i + 1, i + 2] for i in range(len(crdsX) - 1)]
+
+def CreateErrData ( crdsX ):
+
+	errdata = pd.DataFrame(np.array([0.5e-3 for i in range(len(crdsX))]).T, columns = [ 1 ])
+
+	def Create ( crdsX, indexes ):
 		if len(indexes) == 0:
 			return 0
 		if len(indexes) == 1:
@@ -66,24 +56,50 @@ def CreateErrData ( crdsX, indexes ):
 			startX = crdsX[indexes[0]]
 			endX = crdsX[indexes[-1]]
 			lngth = len(indexes)
-			val1 = (errdata.loc[indexes[-1] - 1, lngth - 1] if (errdata.shape[1] >= lngth and errdata.loc[indexes[-1] - 1, lngth - 1] is not None)
-					else CreateErrData(crdsX, indexes[:-1]))
-			val2 = (errdata.loc[indexes[-1], lngth - 1] if (errdata.shape[1] >= lngth and errdata.loc[indexes[-1], lngth - 1] is not None)
-					else CreateErrData(crdsX, indexes[1:]))
+			
+			val1 = (errdata.loc[indexes[-1] - 1, lngth - 1] 
+					if (errdata.shape[1] >= lngth and errdata.loc[indexes[-1] - 1, lngth - 1] is not None)
+					else Create(crdsX, indexes[:-1]))
+			
+			val2 = (errdata.loc[indexes[-1], lngth - 1] 
+					if (errdata.shape[1] >= lngth and errdata.loc[indexes[-1], lngth - 1] is not None)
+					else Create(crdsX, indexes[1:]))
+			
 			val = (val2 + val1) / abs(endX - startX)
 		
 			if lngth not in errdata.columns:
 				errdata[lngth] = None
 
 			errdata.loc[indexes[-1], lngth] = val
-
 			return val
 
+	Create(crdsX, [i for i in range(len(crdsX))])
+	return [errdata.loc[i + 1, i + 2] for i in range(len(crdsX) - 1)]
+
+def Newton ( crdsX, crdsY, x ):
+	res = 0
+	for i in range(len(crdsX) - 1):
+		mult = 1.0
+		for j in range(i + 1):
+			mult *= (x - crdsX[j])
+		res += data[i] * mult
+	res += crdsY[0]
+
+	return res
+
+def AbsError ( crdsX, x ):
+	res = 0
+	for i in range(len(crdsX) - 1):
+		mult = 1.0
+		for j in range(i + 1):
+			mult *= abs(x - crdsX[j])
+		res += errdata[i] * mult
+	res += 0.5e-3
+
+	return res
 	
-CreateData(x, [i for i in range(len(x))])
-CreateErrData(x, [i for i in range(len(x))])
-print('Data : \n', data, '\n')
-print('Error data: \n', errdata, '\n')
+data = CreateData(x, y)
+errdata = CreateErrData(x)
 
 apprX = np.copy(origX)
 apprY = [Newton(x, y, xx) for xx in apprX]
